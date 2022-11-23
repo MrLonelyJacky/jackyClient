247af6aec6d9c857aaf7329803d4b112595b3678
chenjq <15151936001@163.com>
2022-11-23 18:08:09 +0800

    增加handler

--- src/main/java/com/example/springboot/query/JackyClientFactoryBean.java
+++ src/main/java/com/example/springboot/query/JackyClientFactoryBean.java
@@ -6,18 +6,40 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class JackyClientFactoryBean implements FactoryBean<Object>, ApplicationContextAware {
     private ApplicationContext applicationContext;
 
+
+    private Class<?> type;
+
+    private String name;
+
+    private String url;
+
+    private String contextId;
+
+    private String path;
+
     @Override
     public Object getObject() throws Exception {
         JackyClientContext contextBean = applicationContext.getBean(JackyClientContext.class);
-        contextBean.getInstance();
+        //contextBean.getInstance();
         Map<Method, MethodHandler> methodToHandler = new LinkedHashMap<>();
+        Method[] methods = this.type.getMethods();
+        for (Method method : methods) {
+            if (method.getDeclaringClass()==Object.class ){
+                //Object 方法
+                continue;
+            }else {
+                methodToHandler.put(method,new DefaultMethodHandler(method));
+            }
+        }
 
+        Proxy.newProxyInstance()
         return null;
     }
 
