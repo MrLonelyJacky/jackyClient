8c37e53030c17042d844b6b0b7a39100a8305f06
chenjq <15151936001@163.com>
2023-02-18 16:40:02 +0800

    client 启动发布注册事件

---
+++ src/main/java/com/example/springboot/client/DiscoveryClient.java
@@ -0,0 +1,20 @@
+package com.example.springboot.client;
+
+/**
+ * @description: 客户端启动成功时 完成注册、心跳检测、刷新注册列表功能
+ * @author: jacky
+ * @create: 2023-02-18 11:20
+ **/
+public class DiscoveryClient {
+
+
+    public DiscoveryClient() {
+
+        /**
+         * 生成一个事件
+         */
+
+
+
+    }
+}
