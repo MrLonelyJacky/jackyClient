64ae07c39933c24f00f926a2991040d682a75279
MrLonelyJacky <15151936001@163.com>
2024-01-18 12:50:06 +0800

    remove useless code

--- src/main/java/com/jacky/rpc/protocal/SequenceIdGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.jacky.rpc.protocal;
-
-public abstract class SequenceIdGenerator {
-    private static final RedisUtil redisUtil =RedisUtil.getInstance();
-
-    public static int nextId() {
-        return (int) redisUtil.incr("feignResultId",1);
-    }
-}
